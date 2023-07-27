c6a210f60dfee50635e9f38574f2c799af525190
Shane Misra <shane.misra@gmail.com>
2023-07-26 18:16:20 -0600

    router added, no elements fleshed out

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import WelcomePage from '../WelcomePage/WelcomePage';
 import ProjectsPage from '../ProjectsPage/ProjectsPage';
 
+
 function App() {
   return (
     <div className="App">
+
       <Header />
-      <WelcomePage />
-      <ProjectsPage />
+      <Routes>
+      <Route path = '/' element ={<WelcomePage />}/>
+      <Route path = '/projects' element ={<ProjectsPage />} />
+      </Routes>
       <Footer />
     </div>
   );
