8d7942e55c0cb63577bfdecdb2e536120efa16aa
Shane Misra <shane.misra@gmail.com>
2023-09-04 22:20:08 -0600

    enable buttons to routes, small sizing changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import WelcomePage from '../WelcomePage/WelcomePage';
 import ProjectsPage from '../ProjectsPage/ProjectsPage';
+import AboutMe from '../AboutMe/AboutMe';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path = '/' element ={<WelcomePage />}/>
         <Route path = '/projects' element ={<ProjectsPage />} />
+        <Route path = '/aboutme' element = {<AboutMe/>} />
       </Routes>
       </section>
       <Footer />
