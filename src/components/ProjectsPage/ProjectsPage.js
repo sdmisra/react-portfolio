c43fa3ae7a462de628be9d0bafd4353e44d840f8
Shane Misra <shane.misra@gmail.com>
2023-10-13 11:35:44 -0600

    pick up where I left off, set to domain name of previous project

--- src/components/ProjectsPage/ProjectsPage.js
+++ src/components/ProjectsPage/ProjectsPage.js
@@ -3,12 +3,28 @@ import ProjectCard from '../ProjectCard/ProjectCard'
 import '../ProjectsPage/ProjectsPage.css'
 
 const ProjectsPage = ()=> {
+
+  const ProjectArray = {
+    'anthems' : 
+    {title : 'Avian Anthems', size: '4', mod : '3rd', 
+      tech: ['React', 'TypeScript', 'Cypress', 'OOP']
+    },
+    'chronicles' : 
+    {title : 'Mars Chronicles', size: '4', mod : '3rd', 
+      tech : ["React", "Redux", "Cypress", 'Functional']
+    },
+    'hyperloom' : 
+    {title: 'Hyper Loom', size: '7', mod : '4th', 
+      tech: ['React', 'Redux', 'Cypress', 'Functional']
+    }
+  }
+
   return (
     <>
     <div className='project-browser left-side'>
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
+      <ProjectCard project={ProjectArray['anthems']}/>
+      <ProjectCard project={ProjectArray['chronicles']}/>
+      <ProjectCard project={ProjectArray['hyperloom']}/>
     </div>
     <div className='focus-window right-side'>
       <h1>Please choose a project on the left for details</h1>
