ac52a02a11b826600696a29291bcbdff6fa137f0
Shane Misra <shane.misra@gmail.com>
2023-11-09 15:16:51 -0700

    minor changes to center content spacing and links open to tab

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
     <footer className='footer-container'>
       <h4 className='footer-text'>Quick links to my work:</h4>
       <div className='footer-nav-buttons'>
-        <a href='https://hyper-loom-explorer.vercel.app' className='footer-link'>
+        <a href='https://hyper-loom-explorer.vercel.app' className='footer-link' target="_blank" rel="noreferrer noopener">
           Hyper Loom
         </a>
-        <a href='https://avian-anthems.vercel.app' className='footer-link'>
+        <a href='https://avian-anthems.vercel.app' className='footer-link' target="_blank" rel="noreferrer noopener">
           Avian Anthems
         </a>
-        <a href='https://mars-chronicles.vercel.app' className='footer-link'>
+        <a href='https://mars-chronicles.vercel.app' className='footer-link' target="_blank" rel="noreferrer noopener">
           Mars Chronicles
         </a>
       </div>
